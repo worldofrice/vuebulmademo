499a0604fa836bd0d4edcb4f8d910373010c4359
Gert <gert.veereste@gmail.com>
2024-02-29 13:54:40 +0200

    fkme

---
+++ src/components/MessageComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <article :class="`message ${color ? `is-${color}` : ''}`">
+      <div class="message-header">
+        <p>{{ title }}</p>
+      </div>
+      <div class="message-body">
+        {{ message }}
+      </div>
+    </article>
+  </template>
+  
+  <script setup>
+  defineProps({
+    title: {
+        type: String,
+        required: true,
+      },
+      message: {
+        type: String,
+        required: true,
+      },
+      color: {
+        type: String,
+        default: '',
+        validator: function (value) {
+          return ['dark', 'primary', 'link', 'info', 'success', 'warning', 'danger', ''].includes(value);
+        }
+      }
+  });
+  </script>
